2d9476151209c77e45e9df54220dc619c944388b
Kumar Manglam Pandey <manglam8@hotmail.com>
2018-04-12 14:32:22 +0530

    Create terms.jsx

---
+++ client/terms.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+/* ----------  External Libraries  ---------- */
+
+import React from 'react';
+
+/*
+ * Messenger native payments require a link to your company
+ * terms & conditions page.
+ */
+const TermsAndConditions = () => {
+  return (
+    <div className='static-page'>
+      <div className='static-page-body'>
+        <h1>Victoria Belle Terms & Conditions</h1>
+
+        <div className='static-page-subtitle'>
+          <p>Sample terms & conditions page.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TermsAndConditions;

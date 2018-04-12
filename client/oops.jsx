6ca493f4c6c1939d15ddacddd4db655eb692ef8b
Kumar Manglam Pandey <manglam8@hotmail.com>
2018-04-12 14:29:23 +0530

    Create oops.jsx

---
+++ client/oops.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+/* ----------  External Libraries  ---------- */
+
+import React from 'react';
+
+/*
+ * MessengerExtensions are only available on iOS and Android,
+ * so show an error page if MessengerExtensions was unable to start
+ */
+const Oops = () => {
+  return (
+    <div className='static-page'>
+      <div className='static-page-body'>
+        <h1>Please Try in Messenger App</h1>
+
+        <div className='static-page-subtitle'>
+          <p>This experience has been designed to work within the Messenger app on iOS or Android. Please try again there (rather than in a browser).</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Oops;
